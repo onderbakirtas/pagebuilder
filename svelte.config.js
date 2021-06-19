570b1215efa50ff961420d005283a9a6b46b945a
Onder Bakirtas <onderbakirtas@gmail.com>
2021-06-20 01:06:46 +0300

    feat: changed adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,21 @@
+import adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		ssr: true
+		ssr: false,
+		hydrate: false,
+		prerender: {
+			crawl: true,
+			enabled: true
+		},
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
 };
 
